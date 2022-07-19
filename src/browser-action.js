e3cc294ac51ad813616fede5b38fbf910ab23b36
Matthieu Robin <mrobin@ideolys.com>
2022-07-19 15:55:01 +0200

    fix: add other method to detect title

--- src/browser-action.js
+++ src/browser-action.js
@@ -1,5 +1,5 @@
 /**
- * @param {Function} fn 
+ * @param {Function} fn
  * @param {Function} callback results
  */
 function executeScript (fn, callback) {
@@ -17,8 +17,8 @@ function getIssueLabels () {
   const labels = new Set();
   document.querySelectorAll('[data-testid="sidebar-labels"] [data-qa-label-name]').forEach(node => {
     labels.add(node.dataset.qaLabelName);
-  }); 
-  
+  });
+
   return labels;
 }
 
@@ -27,7 +27,7 @@ function getIssueLabels () {
  */
 function getIssueTitle () {
   let title = null;
-  const node = document.querySelector('[data-qa-selector="title_content"]');
+  const node = document.querySelector('[data-qa-selector="title_content"]') || document.querySelector('.qa-title');
 
   if (node) {
     title = node.innerText;
@@ -58,22 +58,22 @@ function getCurrentDate () {
   return new Date().toJSON().slice(0,10);
 }
 
-/** 
- * @param {{ title : {String}, url : {String}, labels : {Set}, milestone : {String} }} variables 
+/**
+ * @param {{ title : {String}, url : {String}, labels : {Set}, milestone : {String} }} variables
  * @returns {String}
  */
 function getTemplateWithFilledValues (variables) {
   return `
       Date : ${ getCurrentDate() }
-      Projet : 
+      Projet :
       Milestone : ${ variables.milestone ? '[[' + variables.milestone + ']]' : '' }
       Source : ${ variables.url }
       Labels : ${ [...variables.labels].map(label => '[[' + label + ']]').join(' ') }
       #git
-      
+
     ---
-    
-    # ${ variables.title } 
+
+    # ${ variables.title }
   `;
 }
 
